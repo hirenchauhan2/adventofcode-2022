7a9d2a61b4145767bb564ad3dfe58638b241fc11
Hiren Chauhan <hiren.maheshwari366@gmail.com>
2022-12-04 16:41:53 +0530

    feat(day-04): camp cleanup part-02

---
+++ day-04/solution-02.js
@@ -0,0 +1,21 @@
+const { readFileSync } = require('fs');
+const { EOL } = require('os');
+
+const input = readFileSync('./input.txt', 'utf-8');
+
+const assignmentPairs = input.split(EOL);
+
+let count = 0;
+
+assignmentPairs.forEach((line) => {
+  const [pair1, pair2] = line.split(',');
+
+  const [start1, end1] = pair1.split('-').map((s) => parseInt(s));
+  const [start2, end2] = pair2.split('-').map((s) => parseInt(s));
+
+  if (!(end1 < start2 || end2 < start1)) {
+    count += 1;
+  }
+});
+
+console.log('Count is', count);
